44b8000b2c3111b0fd5c70adeee1141193a0c185
PhatNguyen203 <Lee0ne203@gmail.com>
2020-09-17 00:41:36 -0400

    set up basic auth router

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,25 @@
 import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navigation from "./components/layout/Navigation";
 import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 import "./App.css";
 
 const App = () => {
   return (
-    <Fragment>
-      <Navigation />
-      <Landing />
-    </Fragment>
+    <Router>
+      <Fragment>
+        <Navigation />
+        <Route exact path='/' component={Landing} />
+        <section className='container'>
+          <Switch>
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/login' component={Login} />
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
   );
 };
 
