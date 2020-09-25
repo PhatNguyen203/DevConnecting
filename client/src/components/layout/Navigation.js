1aebbd36e8a0c8c441c739fabbb2410fdfd2f634
PhatNguyen203 <Lee0ne203@gmail.com>
2020-09-25 00:21:42 -0400

    logout

--- client/src/components/layout/Navigation.js
+++ client/src/components/layout/Navigation.js
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
 
-const Navigation = () => {
+//import redux
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { logout } from "../../actions/auth";
+
+const Navigation = ({ logout, auth: { isAuthenticated } }) => {
+  const authLink = (
+    <ul>
+      <li>
+        <Link to='/profile'>Developers</Link>
+      </li>
+      <li>
+        <Link to='/posts'>Posts</Link>
+      </li>
+      <li>
+        <Link onClick={logout} to='/'>
+          <i className='fas fa-sign-out-alt'>
+            <span className='hide-sm'>Logout</span>
+          </i>
+        </Link>
+      </li>
+    </ul>
+  );
+  const guestLink = (
+    <ul>
+      <li>
+        <Link to='/register'>Register</Link>
+      </li>
+      <li>
+        <Link to='/login'>Login</Link>
+      </li>
+    </ul>
+  );
+
   return (
     <nav className='navbar bg-dark'>
       <h1>
@@ -9,18 +42,16 @@ const Navigation = () => {
           <i className='fas fa-code'></i> DevConnector
         </Link>
       </h1>
-      <ul>
-        <li>
-          <Link to='/'>Developers</Link>
-        </li>
-        <li>
-          <Link to='/register'>Register</Link>
-        </li>
-        <li>
-          <Link to='/login'>Login</Link>
-        </li>
-      </ul>
+      <Fragment>{isAuthenticated ? authLink : guestLink}</Fragment>
     </nav>
   );
 };
-export default Navigation;
+
+Navigation.propTypes = {
+  logout: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+};
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
+export default connect(mapStateToProps, { logout })(Navigation);
