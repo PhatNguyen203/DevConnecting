e29705c096ba3dc910e9c858e99d1441e110c3b3
PhatNguyen203 <Lee0ne203@gmail.com>
2020-09-27 21:32:15 -0400

    profile reducer and get current user profile

--- client/src/actions/constants.js
+++ client/src/actions/constants.js
@@ -7,3 +7,5 @@ export const AUTH_ERROR = "AUTH_ERROR";
 export const USER_LOGIN = "USER_LOGIN";
 export const LOGIN_FAIL = "LOGIN_FAIL";
 export const USER_LOGOUT = "USER_LOGOUT";
+export const GET_PROFILE = "GET_PROFILE";
+export const PROFILE_ERROR = "PROFILE_ERROR";
--- client/src/reducers/root-reducer.js
+++ client/src/reducers/root-reducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import alert from "./alert";
 import auth from "./auth";
+import profile from "./profile";
 
 //NOTE: rootreducers combining all reducers created
 export default combineReducers({
   alert,
   auth,
+  profile,
 });
